dc7898d78231a17204f13b5ff736021096effb23
ShalomJackson <103288376+ShalomJackson@users.noreply.github.com>
2022-08-05 21:49:35 -0600

    Update GS_Creating_Families_SG_2.py

--- GS_Creating_Families_SG_2.py
+++ GS_Creating_Families_SG_2.py
@@ -6,79 +6,80 @@ import pandas as pd
 
 def load_args():
     parser= argparse.ArgumentParser()
-    #parser.add_argument('-v','--verbose')
-    parser.add_argument('-y','--year_to_sample', nargs='+',type=int)
+    parser.add_argument('-y', '--year_to_sample', nargs='+',type=int)
     parser.add_argument('-c', '--census_filepath', type=str)
     parser.add_argument('-o', '--output_prefix', type=str)
+    parser.add_argument('-v', '--verbose', type=bool, default=False)
+    parser.add_argument('-p', '--create_plot', type=bool, default=False)
+    
     
     return parser.parse_args()
 
 
 def family(graph, parent1, curGen, finalGenDepth):
     global curPer
+    global kids_dict
     
-    #print(curGen, finalGenDepth)
     if(curGen >= finalGenDepth):
         return()
     
-    print ("running family: parent1:", parent1, "curPer:", curPer)
-    #print ("current generation", curGen)
-    
-    #print(num_kids)
-    kid = num_kids[curGen] #random number of kids. CHECK HERE
-    #print("# of kids:", kid)
+    # For current parents, will simulate the number of children they have in this gen 
+    kid_num = np.random.choice(kids_dict[years_to_sim[curGen]], size=1)[0] 
     
     parent2 = curPer + 1
     curPer= curPer + 1
-    #print("parent2/curPer:", curPer)
     
-    for i in range(0, kid):
+    if user_args.verbose:
+        print("running family: parent1:", parent1, "curPer:", curPer)
+    
+    for i in range(0, kid_num):
             child = curPer + 1
             curPer = curPer + 1
             
             graph.add_edge(parent1, child)  #edge between parent1 and child (curPer)
-            print ("parent1:", parent1,"kid:", child)
             
             graph.add_edge(parent2, child)
-            print ("parent2:", parent2,"kid:", child)
             
             family(graph, child, curGen + 1, finalGenDepth) #recursion: adds a new generation
           
-    # graph.edges(data=True)
+            if user_args.verbose:
+                
+                print("parent1:", parent1, "kid:", child)
+                print("parent2:", parent2, "kid:", child)
+                
+
     return
 
 if __name__== '__main__':
 
-    user_args= load_args()   
-    years_to_sim= user_args.year_to_sample
-    census_df= pd.read_csv(user_args.census_filepath)
+    user_args = load_args()   
+    years_to_sim = user_args.year_to_sample
+    census_df = pd.read_csv(user_args.census_filepath)
     
     index = 1
     parent1 = index
     fam_graph = nx.MultiDiGraph()
-    cols=[]
+    cols = []
 
  # Check to determine if the years imputed are able to be found in the census file
-    years_isec= np.intersect1d(census_df.columns.astype(int),years_to_sim)
+    years_isec = np.intersect1d(census_df.columns.astype(int),years_to_sim)
+    
     if len(years_to_sim) != len(years_isec):
         print ('EXIT: Years inputted does not match the years provided in the census file')
         exit (0)
     
   # Sample the number id kids to simulate for each generation
-    num_kids = []
+    kids_dict = {} # create a dict where key = generation_year and value = array of #children in generation
     for year in years_to_sim:
-        child_sample= census_df[f'{year}'].sample(n=1).values[0]
-        num_kids.append(child_sample)
-    print(num_kids)
-    
-    start_gen = 0
-    end_gen = len(years_to_sim)
+        kids_dict[year] = census_df[f'{year}'].to_list()
     
     curPer = 1
-    family(fam_graph, parent1, start_gen, end_gen)
+    # Call upon recursive function to simulate family pedigree.
+    family(fam_graph, parent1, 0, len(years_to_sim))
     
+    # save edge list 
     nx.write_edgelist(fam_graph, f"{user_args.output_prefix}.nx") #save edge list
-    #user_args.verbose
     
-    nx.draw(fam_graph,with_labels=True, font_weight='bold')
-    plt.savefig(f"{user_args.output_prefix}.pdf", format='pdf')
+    if user_args.create_plot:
+        nx.draw(fam_graph,with_labels=True, font_weight='bold')
+        plt.savefig(f"{user_args.output_prefix}.pdf", format='pdf')
